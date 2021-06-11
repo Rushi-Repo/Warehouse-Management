be5df76045bdb5dee3eac989ae9e9fac5141c1c7
Rushi <37549697+Rushi-Repo@users.noreply.github.com>
2021-06-11 08:57:44 +0530

    Add files via upload

---
+++ views/templates/footer.php
@@ -0,0 +1,10 @@
+
+  <footer class="main-footer">
+
+  Back-end developed by Rushikesh Kshirsagar & Garima Mathur.<br>Front-end Code snippets borrowed from open source "InventorySystem" by RonKnight(Github)
+
+   </footer>
+
+
+
+
